c4e7cb687d3e37cd53f217843f211f99c9e8acc1
renan00923 <renan.aguiar2@aluno.ce.gov.br>
2025-04-11 14:56:01 -0300

    Add files via upload

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,48 +1,58 @@
-var cont = 1;
-var ganhador = false
-var jogador = 'X'; 
-var maquina = 'O'; 
-var exibeTabela = document.getElementById('tabela');
-var vencedor = null;
-var dificuldade = 'facil';
+/*
+	O código foi baseado essencialmente em .target, getAtrribute, querySelector, data-*, display: none e display: flex, .reload
+*/
 
+var cont = 1; // contador recebe 1
+var ganhador = false // ganhador
+var jogador = 'X';  // jogador recebe o X
+var maquina = 'O';  // maquina recebe O
+var exibeTabela = document.getElementById('tabela'); // variável para exibir a tabela
+var vencedor = null; // variável 
+var dificuldade = 'facil'; // dificuldade começa como fácil
+
+// tabela espelho do jogo
 var tabela = [
 	['', '', ''],
 	['', '', ''],
 	['', '', '']
 ];
-
+// combinações em sequência de 3*3 para definir se alguém ganhou
 var combinacoesVencedoras = [
-	[[0, 0], [0, 1], [0, 2]],
-	[[1, 0], [1, 1], [1, 2]],
-	[[2, 0], [2, 1], [2, 2]],
-	[[0, 0], [1, 0], [2, 0]],
-	[[0, 1], [1, 1], [2, 1]],
-	[[0, 2], [1, 2], [2, 2]],
-	[[0, 0], [1, 1], [2, 2]],
-	[[0, 2], [1, 1], [2, 0]]
+	[[0, 0], [0, 1], [0, 2]], // combinacao[0]
+	[[1, 0], [1, 1], [1, 2]], // combinacao[1]
+	[[2, 0], [2, 1], [2, 2]], // combinacao[2]
+	[[0, 0], [1, 0], [2, 0]], // combinacao[3]
+	[[0, 1], [1, 1], [2, 1]], // combinacao[4]
+	[[0, 2], [1, 2], [2, 2]], // combinacao[5]
+	[[0, 0], [1, 1], [2, 2]], // combinacao[6]
+	[[0, 2], [1, 1], [2, 0]]  // combinacao[7]
 ];
 
+// funcao inicial para o jogador escolher seu personagem
 function escolherPersonagem(event) {
-	const click = event.target;
-	var personagem = click.getAttribute("data-personagem");
-	var inicio = document.getElementById('inicio');
+	const click = event.target; // a constante click pega o elemento selecionado
+	var personagem = click.getAttribute("data-personagem"); // o personagem vai receber o botao selecionado onde será lido o atributo data-personagem atribuido no html
+	var inicio = document.getElementById('inicio'); // variável pega o id inicio para futuramente atribuir display: none
 	
+	// se o jogador escolher explosao
 	if (personagem == 'explosao') {
 		jogador = 'X';
 		maquina = 'O';
 		cont = 1;
 	}
+	// se o jogador escolher bomba
 	else if (personagem == 'bomba') {
 		jogador = 'O';
 		maquina = 'X';
 	}
+	// após a escolha, entra a tabela em cena
 	inicio.style.display = 'none';
 	exibeTabela.style.display = 'flex';
 }
+// parte essencial do jogo, onde as coisas começam
 function jogo(event) {
-    const click = event.target;
-    var img = click.querySelector('img');
+    const click = event.target; // constante click pega o elemento selecionado
+    var img = click.querySelector('img'); // a img vai receber o local da imagem clicada
     const row = parseInt(click.getAttribute("data-row"));
     const col = parseInt(click.getAttribute("data-col"));
 
@@ -144,7 +154,7 @@ function jogaMaquina() {
 			var posicao = celulasVazias[escolha];
 			var row = posicao[0];
 			var col = posicao[1];
-	
+			
 			tabela[row][col] = maquina;
 			
 			var img = document.querySelector(`[data-row="${row}"][data-col="${col}"] img`);
